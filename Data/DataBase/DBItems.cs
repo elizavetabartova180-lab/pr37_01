9ecca2ffd89d102be01e31617416855e31cb5841
Kik02 <Golovin.Savelii@yandex.ru>
2026-05-16 16:45:56 +0500

    Получение данных о предметах. Бартова

---
+++ Data/DataBase/DBItems.cs
@@ -0,0 +1,37 @@
+﻿using MySql.Data.MySqlClient;
+using Shop_Bartova.Data.Common;
+using Shop_Bartova.Data.Interfaces;
+using Shop_Bartova.Data.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shop_Bartova.Data.DataBase
+{
+    public class DBItems :IItems
+    {
+        public IEnumerable<Categorys> Categorys = new DBCategory().AllCategorys;
+        public IEnumerable<Items> AllItems
+        {
+            get
+            {
+                List<Items> items = new List<Items>();
+                MySqlConnection MySqlConnection = Connection.MySqlOpen();
+                MySqlDataReader ItemsData = Connection.MySqlQuery("SELECT*FROM Shop.items ORDER BY 'Name';", MySqlConnection);
+                while (ItemsData.Read())
+                {
+                    items.Add(new Items()
+                    {
+                        Id = ItemsData.IsDBNull(0) ? -1 : ItemsData.GetInt32(0),
+                        Name = ItemsData.IsDBNull(1) ? "" : ItemsData.GetString(1),
+                        Description = ItemsData.IsDBNull(2) ? "" : ItemsData.GetString(2),
+                        Img = ItemsData.IsDBNull(3) ? "" : ItemsData.GetString(3),
+                        Price = ItemsData.IsDBNull(4) ? -1 : ItemsData.GetInt32(4),
+                        Category = ItemsData.IsDBNull(5) ? null : Categorys.Where(x=>x.Id == ItemsData.GetInt32(5)).First()
+                    });
+                }
+                MySqlConnection.Close();
+                return items;
+            }
+        }
+    }
+}
