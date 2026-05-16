6175321baac23a7dd9a66eeef477e9394a780aa3
Kik02 <Golovin.Savelii@yandex.ru>
2026-05-16 16:26:51 +0500

    Разработанное подключение к MySql.  Бартова

---
+++ Data/Common/Connection.cs
@@ -0,0 +1,26 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace Shop_Bartova.Data.Common
+{
+    public class Connection
+    {
+        readonly static string ConnectionData = "server=127.0.0.1; port = 3306; database = Shop; uid=root; pwd=;";
+
+        public static MySqlConnection MySqlOpen()
+        {
+            MySqlConnection NewMySqlConnection = new MySqlConnection(ConnectionData);
+            NewMySqlConnection.Open();
+
+            return NewMySqlConnection;
+        }
+        public static MySqlDataReader MySqlQuery(string Query, MySqlConnection Connection)
+        {
+            MySqlCommand NewMySqlCommand = new MySqlCommand(Query, Connection);
+            return NewMySqlCommand.ExecuteReader();
+        }
+        public static void MySqlClose(MySqlConnection connection)
+        {
+            connection.Close();
+        }
+    }
+}
