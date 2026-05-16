642b9f837bb64de636d4f61933e67be47929a5a6
ADMIN <ADMIN@MSI>
2026-05-17 01:12:56 +0500

    Метод добавления предмета. Бартова

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -1,21 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Shop_Bartova.Data.Interfaces;
 using Shop_Bartova.Data.Models;
 using Shop_Bartova.Data.ViewModell;
-using System.Collections;
+using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Shop_Bartova.Controllers
 {
     public class ItemsController : Controller
     {
+        private readonly IHostingEnvironment hostingEnvironment;
         private IItems IAllItems;
         private ICategorys IAllCategorys;
         VMItems VMItems = new VMItems();
-        public ItemsController(IItems IAllItems, ICategorys IAllCategorys)
+        public ItemsController(IItems IAllItems, ICategorys IAllCategorys, IHostingEnvironment environment)
         {
             this.IAllItems = IAllItems;
             this.IAllCategorys = IAllCategorys;
+            this.hostingEnvironment = environment;
         }
         public ViewResult List(int id=0)
         {
@@ -33,5 +38,25 @@ namespace Shop_Bartova.Controllers
             IEnumerable<Categorys> Categorys = IAllCategorys.AllCategorys;
             return View(Categorys);
         }
+        [HttpPost]
+        public RedirectResult Add(string name, string description, IFormFile files, float price, int idCategory)
+        {
+            if (files != null)
+            {
+                var uploads = Path.Combine(hostingEnvironment.WebRootPath, "img");
+                var filePath = Path.Combine(uploads, files.FileName);
+                files.CopyTo(new FileStream(filePath, FileMode.Create));
+            }
+            Items newItems = new Items();
+            newItems.Name = name;
+            newItems.Description = description;
+            newItems.Img = files.FileName;
+            newItems.Price = Convert.ToInt32(price);
+            newItems.Category = new Categorys() { Id = idCategory };
+            int id = IAllItems.Add(newItems);
+            return Redirect("/Items/Update?id=" + id);
+
+        }
+
     }
 }
