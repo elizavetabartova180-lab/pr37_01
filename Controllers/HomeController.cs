2924f789a5f7a283e732c7022c9377141f4880b4
Kik02 <Golovin.Savelii@yandex.ru>
2026-05-21 08:56:10 +0500

    Разработанное перенаправление на страницу с предметами. Бартова

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,9 +4,9 @@ namespace Shop_Bartova.Controllers
 {
     public class HomeController : Controller
     {
-        //public RedirectResult Index()
-        //{
-        //    return Redirect("/Items/List");
-        //}
+        public RedirectResult Index()
+        {
+            return Redirect("/Items/List");
+        }
     }
 }
